88f5f09bc1d94014379ecf5094f731a20e82a366
Junebeom Lee <junebeom.dev@gmail.com>
2025-01-29 17:13:13 +0900

    Initial Commit

---
+++ xcode-cli-exmaple-01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode-cli-exmaple-01
+//
+//  Created by junebeom on 2025/01/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
